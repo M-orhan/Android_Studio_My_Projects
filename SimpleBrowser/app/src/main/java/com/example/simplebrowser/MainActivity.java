60cf06b529e48e849ecf9caa667d24d50c32ebfd
M-orhan <154002631+M-orhan@users.noreply.github.com>
2024-03-01 18:01:40 +0300

    A simple browser design that accesses the site when the address extension is typed in the browser bar.

---
+++ SimpleBrowser/app/src/main/java/com/example/simplebrowser/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.simplebrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webview;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview=(WebView)findViewById(R.id.webview);
+        editText=(EditText)findViewById(R.id.edittext);
+
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String url = editText.getText().toString();
+                if (!url.isEmpty()) {
+                    webview.loadUrl(url);
+                }
+            }
+        });
+
+        webview.loadUrl("https://www.google.com");
+    }
+
+}
